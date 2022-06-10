3ce9022fecfcbd304a869c23e7fb1a2cbb6a51cd
ladyTIC <kadidiatoucoulibaly013@gmail.com>
2022-06-10 11:55:06 +0000

    maj

---
+++ message.js
@@ -0,0 +1,33 @@
+// new code
+function removeItems() {
+    document.getElementById('nom').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('objet').value = '';
+    document.getElementById('message').value = '';
+}
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function (e) {
+    e.preventDefault()
+    var nom = document.getElementById('nom').value;
+    var email = document.getElementById('email').value;
+    var objet = document.getElementById('objet').value;
+    var message = document.getElementById('message').value;
+    var body = '' + nom + '<br>' + email + '<br>' +
+        objet + '<br>' + message;
+
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "kadidiatoucoulibaly013@gmail.com",
+        Password: "fudwfbijnxtqugoj",
+        To: 'kadidiatoucoulibaly013@gmail.com',
+        From: email,
+        Subject: objet,
+        Body: body,
+    }).then(
+      //  message => alert('message envoyer')
+        console.log('ok')
+    )
+    removeItems();
+})
--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer Web", "Network Administrator", "Computer Maintainer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Developer web", "Network Administrator", "Computer Maintainer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,3 +71,6 @@ $(document).ready(function(){
         }
     });
 });
+
+
+   
